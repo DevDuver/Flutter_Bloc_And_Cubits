37df86cde771e3a0d337812691a69979cb6f02a2
Duver Melo <duvermelo2606@gmail.com>
2023-11-29 10:44:47 -0500

    implementacion basica de validaciones en formulario tradicional

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forms_app/presentation/widgets/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -19,6 +20,106 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: 	SingleChildScrollView(
+          child: Column(
+            children: [
+              FlutterLogo(size: 100),
+
+              _RegisterForm(),
+
+              SizedBox(height: 20),
+
+              SizedBox(height: 20)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RegisterForm extends StatefulWidget {
+  const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  String username = '';
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          
+          CustomTextFormField(
+            label: 'Nombre de usuario',
+            onChange: (value) => username = value,
+            validator: (value) {
+              if ( value == null || value.isEmpty ) return 'El nombre de usuario es requerido';
+              if ( value.trim().isEmpty ) return 'El nombre de usuario es requerido';
+              if ( value.trim().length < 6 ) return 'El nombre de usuario debe tener mas de 6 letras';
+              return null;
+            },
+          ),
+          
+          const SizedBox(height: 10),
+          
+          CustomTextFormField(
+            label: 'Correo eléctronico',
+            onChange: (value) => email = value,
+            validator: (value) {
+              if ( value == null || value.isEmpty ) return 'El correo es requerido';
+              if ( value.trim().isEmpty ) return 'El correo es requerido';
+              
+              final emailRegExp = RegExp(
+                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+              );
+              if ( !emailRegExp.hasMatch(value) ) return 'Este correo no es valido';
+
+              return null;
+            },
+          ),
+
+          const SizedBox(height: 10),
+
+          CustomTextFormField(
+            label: 'Contraseña',
+            obscureText: true,
+            onChange: (value) => password = value,
+            validator: (value) {
+              if ( value == null || value.isEmpty ) return 'La contraseña es requerida';
+              if ( value.trim().isEmpty ) return 'La contraseña es requerida';
+              if ( value.trim().length < 6 ) return 'La contraseña debe tener mas de 6 letras';
+
+              return null;
+            },
+          ),
+
+          const SizedBox(height: 20),
+
+          FilledButton.tonalIcon(
+            onPressed: () {
+              final bool isValid = _formKey.currentState!.validate();
+              if (!isValid) return;
+
+              print('$username, $email, $password');
+            }, 
+            icon: const Icon(Icons.save),
+            label: const Text('Crear usuario'),
+          ),
+        ],
+      )
+    );
   }
 }
---
+++ lib/presentation/widgets/inputs/custom_text_form_filed.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final Function(String)? onChange;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+
+  const  CustomTextFormField({
+    super.key,
+    this.label, 
+    this.hint, 
+    this.errorMessage, 
+    this.onChange, 
+    this.validator,
+    this.obscureText = false
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(color: colors.primary),
+      borderRadius: BorderRadius.circular(40)
+    );
+
+    return TextFormField(
+      onChanged: onChange,
+      validator: validator,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        enabledBorder: border,
+        focusedBorder: border.copyWith(borderSide: BorderSide(color: colors.primary)),
+        errorBorder: border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        focusedErrorBorder: border.copyWith(borderSide: BorderSide(color: colors.primary)),
+
+        isDense: true,
+        label: label != null ? Text(label!) : null,
+        hintText: hint,
+        focusColor: colors.primary,
+        errorText: errorMessage,
+        //prefixIcon: Icon(Icons.insert_chart, color: colors.primary)
+      ),
+    );
+  }
+}
